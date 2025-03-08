05d50b4ed55b3d0715c8cb4dc4e03dfdcc06d1b1
l3117231760 <3117231760@qq.com>
2025-03-08 19:43:57 +0800

    Multithreading Uthread: switching between threads is OK

--- user/uthread.c
+++ user/uthread.c
@@ -10,10 +10,32 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct thread_context
+{
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
+
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
+//  void       (*func)();
+  struct thread_context context;
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
@@ -62,6 +84,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch((uint64)&t->context,(uint64)&current_thread->context);
   } else
     next_thread = 0;
 }
@@ -74,7 +97,12 @@ thread_create(void (*func)())
   for (t = all_thread; t < all_thread + MAX_THREAD; t++) {
     if (t->state == FREE) break;
   }
+  t->context.ra = (uint64)func;
+  t->context.sp = (uint64)t->stack+MAX_THREAD-1;
+  // t->context.s0 = (uint64)t->stack;
   t->state = RUNNABLE;
+  // (uint64)t->stack+MAX_THREAD
+  // (uint64)t->stack
   // YOUR CODE HERE
 }
 
